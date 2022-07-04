af65b3840b8253749a68ae50b8dab245e7899489
Viacheslav-Trifonov <106872149+Viacheslav-Trifonov@users.noreply.github.com>
2022-07-04 12:00:02 +0400

    Delete test_items.py

--- test_items.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    element1 = browser.find_elements(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
-    assert len(element1) > 0, 'Button not found' 
